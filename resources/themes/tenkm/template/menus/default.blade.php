ed68292e14ca0c9383afa9b5fe3f14556eeb165d
levinh <lethevinh.qng@gmail.com>
2020-11-20 14:13:28 +0700

    update menu main

--- resources/themes/tenkm/template/menus/default.blade.php
+++ resources/themes/tenkm/template/menus/default.blade.php
@@ -1,9 +1,9 @@
-<h4 class="widget-title">{{$menu->title_lb}}</h4>
+<h4 class="widget-title">{{$menu['title_lb']}}</h4>
 <ul>
-    @foreach($menu->children()->orderBy('order_nb')->get() as $item)
-        <li @if($item->current()) class="current-menu-item" @endif>
-            <a href="{{$item->url_lb}}">
-                {{$item->title_lb}}
+    @foreach($menu['children'] as $item)
+        <li>
+            <a href="{{$item['url_lb']}}">
+                {{$item['title_lb']}}
             </a>
         </li>
     @endforeach
