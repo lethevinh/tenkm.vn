5f96d0d04c7172b2d460b6d59bf239e962bf57d5
vinh.lethe <vinh.lethe@lazada.com>
2021-01-09 19:55:36 +0700

    update add link location

--- resources/themes/tenkm/template/pages/contact.blade.php
+++ resources/themes/tenkm/template/pages/contact.blade.php
@@ -37,7 +37,7 @@
                 <div class="col-lg-8">
                     <div class="contact-page-map">
                         @if(option('map_iframe'))
-                            {{option('map_iframe')}}
+                            {!! option('map_iframe') !!}
                         @else
                         <iframe class="w-100"
                                 src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d60021.82409444856!2d-122.40118071595978!3d37.7546723469594!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2sbd!4v1577786376747!5m2!1sen!2sbd"
