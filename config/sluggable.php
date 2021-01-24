b2974df15b2d3dbe2c2736935d358f8b6bdae139
vinh.lethe <vinh.lethe@lazada.com>
2021-01-24 19:45:46 +0700

    update view area

--- config/sluggable.php
+++ config/sluggable.php
@@ -87,7 +87,7 @@ return [
      * "similar" slugs.  The closure should return the new unique
      * suffix to append to the slug.
      */
-    
+
     'uniqueSuffix' => null,
 
     /**
@@ -135,7 +135,12 @@ return [
      * is probably not a good idea from an SEO point of view.
      * Only set this to true if you understand the possible consequences.
      */
-    
+
     'onUpdate' => false,
+    /**
+     * If the default slug engine of cocur/slugify is used, this array of
+     * configuration options will be used when instantiating the engine.
+     */
+    'slugEngineOptions' => [],
 
 ];
