3c4c37e9abb6f76881c0c6483f0d343325496866
Le Vinh <levinh@ViMacPro.local>
2021-04-13 21:57:25 +0700

    update client views

--- resources/themes/tenkm/template/collection/client.blade.php
+++ resources/themes/tenkm/template/collection/client.blade.php
@@ -1,6 +1,6 @@
 <div class="client-review-img">
     @foreach($clients as $key => $client)
-        <img class="clr-img clr-img{{$key}}" src="{{ $client->thumbnail }}" alt="{{ $client->title_lb }}">
+        <img class="clr-img clr-img{{$key + 1}}" src="{{ $client->thumbnail }}" alt="{{ $client->title_lb }}">
     @endforeach
     <div class="row justify-content-center">
         <div class="col-xl-6 col-lg-7 col-md-10">
